127672fe5f32ae8958905c11f0cb17a2232090c5
Farkas Tibor <32579497+theseur@users.noreply.github.com>
2023-11-15 19:11:29 +0100

    ez még működik is

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 //alkalmaz�s gy�k�r k�nyvt�ra a szerveren
-define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/vizvezetek/');
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/web2_vizvezetekszerelok2/');
 
 //URL c�m az alkalmaz�s gy�ker�hez
-define('SITE_ROOT', 'http://localhost/vizvezetek/');
+define('SITE_ROOT', 'http://localhost/web2_vizvezetekszerelok2/');
 
 // a router.php vez�rl� bet�lt�se
 require_once(SERVER_ROOT . 'controllers/' . 'router.php');
